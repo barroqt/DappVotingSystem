e10564c6ad1f514ec35b43edd302a917edea9bc6
barroqt <barroqt@gmail.com>
2022-12-27 14:37:04 +0100

    workflow tests

--- test/voting.test.js
+++ test/voting.test.js
@@ -43,8 +43,8 @@ const { developmentChains } = require("../helper-hardhat-config");
           ).to.be.revertedWith("Ownable: caller is not the owner");
         });
 
-        it("should revert with an error if voter registration is closed", async function () {
-          // Set status to the next stage
+        it("should revert with an error if voter registration phase is not open", async function () {
+          // Set workflow status to the next stage
           await voting.startProposalsRegistering();
           await expect(voting.addVoter(voter1.address)).to.be.revertedWith(
             "Voters registration is not open yet"
@@ -81,7 +81,6 @@ const { developmentChains } = require("../helper-hardhat-config");
         });
 
         it("should add a proposal", async function () {
-          console.log((await voting.workflowStatus()).toString());
           await expect(
             voting.connect(voter1).addProposal("Proposal number 1")
           ).to.emit(voting, "ProposalRegistered");
@@ -98,8 +97,8 @@ const { developmentChains } = require("../helper-hardhat-config");
           ).to.be.revertedWith("You're not a voter");
         });
 
-        it("should revert with an error if proposal registration is closed", async function () {
-          // Set status to the next stage
+        it("should revert with an error if proposal registration phase is not open", async function () {
+          // Set workflow status to the next stage
           await voting.endProposalsRegistering();
           await expect(
             voting.connect(voter1).addProposal("Proposal number 1")
@@ -126,50 +125,176 @@ const { developmentChains } = require("../helper-hardhat-config");
       //////////////////////////////
 
       describe("Set vote", function () {
-        it("should cast a vote", async function () {});
+        beforeEach(async () => {
+          await deployments.fixture(["Voting"]);
+          voting = await ethers.getContract("Voting");
+          // Add 3 voters
+          await voting.addVoter(voter1.address);
+          await voting.addVoter(voter2.address);
+          await voting.addVoter(voter3.address);
 
-        it("should revert with an error if the caller is not whitelisted", async function () {});
+          // start proposal phase
+          await voting.startProposalsRegistering();
 
-        it("should revert with an error if the caller has already voted", async function () {});
+          // Add one proposal per voter
+          await voting.connect(voter1).addProposal("Proposal number 1");
+          await voting.connect(voter2).addProposal("Proposal number 2");
+          await voting.connect(voter3).addProposal("Proposal number 3");
 
-        it("should revert with an error if the proposal does not exist", async function () {});
+          // end proposal phase and start voting session
+          await voting.endProposalsRegistering();
+          await voting.startVotingSession();
+        });
+
+        // it("should cast a vote", async function () {
+        //   await expect(voting.connect(voter1).setVote(3)).to.emit(
+        //     voting,
+        //     "Voted"
+        //   );
+
+        //   assert(voting.voters[voter1].votedProposalId === 3);
+        //   assert(voting.voters[voter1].hasVoted === true);
+
+        //   // add votes to the 3rd proposal
+        //   await voting.connect(voter2).setVote(3);
+        //   await voting.connect(voter3).setVote(3);
+        //   assert(voting.proposalsArray[3].voteCount === 3);
+        // });
+
+        // it("should revert with an error if the caller is not whitelisted", async function () {
+        //   await expect(voting.setVote(1)).to.be.revertedWith(
+        //     "You're not a voter"
+        //   );
+        // });
+
+        it("should revert with an error if the caller has already voted", async function () {
+          // Vote once then expect the second vote to fail
+          await voting.connect(voter1).setVote(3);
+          await expect(voting.connect(voter1).setVote(3)).to.be.revertedWith(
+            "You have already voted"
+          );
+        });
+
+        it("should revert with an error if the proposal does not exist", async function () {
+          await expect(voting.connect(voter1).setVote(4)).to.be.revertedWith(
+            "Proposal not found"
+          );
+        });
+
+        it("should revert with an error if the voting session is not open", async function () {
+          // Set workflow status to the next stage
+          await voting.endVotingSession();
+          await expect(voting.connect(voter1).setVote(2)).to.be.revertedWith(
+            "Voting session havent started yet"
+          );
+        });
       });
 
       //////////////////////////////
       //  State
       //////////////////////////////
+      describe("Workflow state", function () {
+        beforeEach(async () => {
+          await deployments.fixture(["Voting"]);
+          voting = await ethers.getContract("Voting");
+        });
 
-      describe("Start proposal registering", function () {
-        it("should set a genesis proposal", async function () {});
+        it("should go through the entire workflow", async function () {
+          // console.log((await voting.workflowStatus()).toString());
+          await expect(voting.startProposalsRegistering()).to.emit(
+            voting,
+            "WorkflowStatusChange"
+          );
 
-        it("should change the workflow status to ProposalsRegistrationStarted", async function () {});
+          // Test enum value to confirm the right workflow status is set
+          let currentState = await voting.workflowStatus();
+          assert(currentState.toString() === "1");
 
-        it("should revert with an error if the current workflow status is not RegisteringVoters", async function () {});
-      });
+          await expect(voting.endProposalsRegistering()).to.emit(
+            voting,
+            "WorkflowStatusChange"
+          );
 
-      describe("End proposal registering", function () {
-        it("should change the workflow status to ProposalsRegistrationEnded", async function () {});
+          // Test enum value to confirm the right workflow status is set
+          currentState = await voting.workflowStatus();
+          assert(currentState.toString() === "2");
 
-        it("should revert with an error if the current workflow status is not ProposalsRegistrationStarted", async function () {});
-      });
+          await expect(voting.startVotingSession()).to.emit(
+            voting,
+            "WorkflowStatusChange"
+          );
 
-      describe("Start voting session", function () {
-        it("should change the workflow status to VotingSessionStarted", async function () {});
+          // Test enum value to confirm the right workflow status is set
+          currentState = await voting.workflowStatus();
+          assert(currentState.toString() === "3");
 
-        it("should revert with an error if the current workflow status is not ProposalsRegistrationEnded", async function () {});
-      });
+          await expect(voting.endVotingSession()).to.emit(
+            voting,
+            "WorkflowStatusChange"
+          );
 
-      describe("End voting session", function () {
-        it("should change the workflow status to VotingSessionEnded", async function () {});
+          // Test enum value to confirm the right workflow status is set
+          currentState = await voting.workflowStatus();
+          assert(currentState.toString() === "4");
 
-        it("should revert with an error if the current workflow status is not VotingSessionStarted", async function () {});
-      });
+          await expect(voting.tallyVotes()).to.emit(
+            voting,
+            "WorkflowStatusChange"
+          );
 
-      describe("Tally votes", function () {
-        it("should store the proposal with the most votes", async function () {});
+          // Test enum value to confirm the right workflow status is set
+          currentState = await voting.workflowStatus();
+          assert(currentState.toString() === "5");
+        });
 
-        it("should change the workflow status to VotesTallied", async function () {});
+        describe("Start proposal registering", function () {
+          it("should set a genesis proposal", async function () {});
 
-        it("should revert with an error if the current workflow status is not VotingSessionEnded", async function () {});
+          it("should revert with an error if the current workflow status is not RegisteringVoters", async function () {
+            // Change workflow to fail second try
+            await voting.startProposalsRegistering();
+            await expect(voting.startProposalsRegistering()).to.be.revertedWith(
+              "Registering proposals cant be started now"
+            );
+          });
+        });
+
+        describe("End proposal registering", function () {
+          it("should revert with an error if the current workflow status is not ProposalsRegistrationStarted", async function () {
+            // Fails at first try because the state has never been updated
+            await expect(voting.endProposalsRegistering()).to.be.revertedWith(
+              "Registering proposals havent started yet"
+            );
+          });
+        });
+
+        describe("Start voting session", function () {
+          it("should revert with an error if the current workflow status is not ProposalsRegistrationEnded", async function () {
+            // Fails at first try because the state has never been updated
+            await expect(voting.startVotingSession()).to.be.revertedWith(
+              "Registering proposals phase is not finished"
+            );
+          });
+        });
+
+        describe("End voting session", function () {
+          it("should revert with an error if the current workflow status is not VotingSessionStarted", async function () {
+            // Fails at first try because the state has never been updated
+            await expect(voting.endVotingSession()).to.be.revertedWith(
+              "Voting session havent started yet"
+            );
+          });
+        });
+
+        describe("Tally votes", function () {
+          it("should store the proposal with the most votes", async function () {});
+
+          it("should revert with an error if the current workflow status is not VotingSessionEnded", async function () {
+            // Fails at first try because the state has never been updated
+            await expect(voting.tallyVotes()).to.be.revertedWith(
+              "Current status is not voting session ended"
+            );
+          });
+        });
       });
     });
